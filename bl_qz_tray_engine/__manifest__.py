a83df958342199e9897e41b0c8e9e32e6971daa3
Black-Labs <federico.bernis@alltechweb.com.ar>
2026-08-25 02:45:10 +0000

    feat(qz): unificar boton detectar con estilo azul
    Actualiza el widget bl_qz_printer para usar boton btn-primary con icono de lupa y texto Detectar impresoras, manteniendo logica de deteccion. Bump 18.0.1.6.6.

--- bl_qz_tray_engine/__manifest__.py
+++ bl_qz_tray_engine/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "BlackLabs QZ Tray Engine",
-    "version": "18.0.1.7.0",
+    "version": "18.0.1.7.1",
     "author": "Ernesto Bernís - BlackLabs",
     "license": "LGPL-3",
     "category": "Point Of Sale",
