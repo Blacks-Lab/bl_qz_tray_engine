45bb961edc48d29141f6d598a1b6db58c5513d1c
Black-Labs <federico.bernis@alltechweb.com.ar>
2026-08-25 03:37:40 +0000

    [IMP] bl_qz_tray_engine: unify detect printer button markup

--- bl_qz_tray_engine/__manifest__.py
+++ bl_qz_tray_engine/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "BlackLabs QZ Tray Engine",
-    "version": "18.0.1.7.1",
+    "version": "18.0.1.7.2",
     "author": "Ernesto Bernís - BlackLabs",
     "license": "LGPL-3",
     "category": "Point Of Sale",
